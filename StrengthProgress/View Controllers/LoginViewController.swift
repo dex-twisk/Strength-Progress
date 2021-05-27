fb134e674641f4ab60047b1b83381a9cf168cdfe
dex-twisk <dextwisk@kpnmail.nl>
2021-05-27 13:57:29 +0200

    1st start to the code

---
+++ StrengthProgress/View Controllers/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  StrengthProgress
+//
+//  Created by Dex Twisk on 24/05/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                    
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+}
---
+++ StrengthProgress/View Controllers/SignUpViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SignUpViewController.swift
+//  StrengthProgress
+//
+//  Created by Dex Twisk on 24/05/2021.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var fullNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var confirmTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+    
+}
